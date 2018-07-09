ab58db4af4be1e38565fa1eac3b7df0d41839feb
YouChaoMin <1330676845@qq.com>
2018-07-10 01:32:16 +0800

    233

---
+++ UICustomViews/app/src/main/java/com/example/ycm/uicustomviews/TitleLayout.java
@@ -0,0 +1,36 @@
+package com.example.ycm.uicustomviews;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * Created by YCM on 2018/7/10.
+ */
+
+public class TitleLayout extends LinearLayout {
+    public TitleLayout(Context context, AttributeSet attrs){
+        super(context,attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button titleBack=(Button)findViewById(R.id.title_back);
+        Button titleEdit=(Button)findViewById(R.id.title_edit);
+        titleBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity)getContext()).finish();
+            }
+        });
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"You clicked Edit button",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
