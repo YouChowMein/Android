66a52ed9da7dd9458ce331ec69c88bce8006ec79
YouChaoMin <1330676845@qq.com>
2018-07-20 16:37:15 +0800

    233

--- BroadcastBestPractice/app/src/main/java/com/example/ycm/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/ycm/broadcastbestpractice/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.ycm.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,17 +15,31 @@ import android.widget.Toast;
  */
 
 public class LoginActivity extends BaseActivity {
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+    private CheckBox rememberPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        pref= PreferenceManager.getDefaultSharedPreferences(this);
         accountEdit=(EditText) findViewById(R.id.account);
         passwordEdit=(EditText) findViewById(R.id.password);
+        rememberPass=(CheckBox) findViewById(R.id.remember_pass);
         login=(Button) findViewById(R.id.login);
+        boolean isRemember=pref.getBoolean("remember_password",false);
+        if(isRemember){
+            //将账号和密码都设置到文本框中
+            String accout=pref.getString("account","");
+            String password=pref.getString("password","");
+            accountEdit.setText(accout);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -30,6 +47,15 @@ public class LoginActivity extends BaseActivity {
                 String password=passwordEdit.getText().toString();
                 //如果账号是admin且密码是123456,就认为登录成功
                 if(accout.equals("admin")&&password.equals("123456")){
+                    editor=pref.edit();
+                    if(rememberPass.isChecked()) {//检查复选框是否被选中
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", accout);
+                        editor.putString("password", password);
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
                     Intent intent=new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
