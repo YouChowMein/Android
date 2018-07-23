11f6a8bd12dad1ac956e68e58ca11c4bbe74b208
YouChaoMin <1330676845@qq.com>
2018-07-24 01:01:19 +0800

    233

---
+++ app/src/main/java/com/example/ycm/litepaltest/Book.java
@@ -0,0 +1,53 @@
+package com.example.ycm.litepaltest;
+
+/**
+ * Created by YCM on 2018/7/23.
+ */
+
+public class Book {
+    private int id;
+    private String author;
+    private double price;
+    private int pages;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
