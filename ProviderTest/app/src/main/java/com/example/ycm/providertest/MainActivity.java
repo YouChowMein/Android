33a4062a8bb0a090c5536c1f44b46305c0baee42
YouChaoMin <1330676845@qq.com>
2018-07-25 19:29:35 +0800

    233

--- ProviderTest/app/src/main/java/com/example/ycm/providertest/MainActivity.java
+++ ProviderTest/app/src/main/java/com/example/ycm/providertest/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 values.put("name","A Clash of Kings");
                 values.put("author","George Martin");
                 values.put("pages",1040);
-                values.put("price",22.85);
+                values.put("price",55.55);
                 Uri newUri=getContentResolver().insert(uri,values);
                 newId=newUri.getPathSegments().get(1);
             }
